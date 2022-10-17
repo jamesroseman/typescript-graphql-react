569f3b75ed73ff27a7242e1231a2dbf11d5a5ea6
James Roseman <james.roseman@gmail.com>
2022-10-17 13:18:46 +0100

    Function deployment is working as expected.

---
+++ src/api/server/createHandler.ts
@@ -0,0 +1,21 @@
+import { ApolloServer } from 'apollo-server-cloud-functions';
+import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
+
+import createSchema from './createSchema';
+
+/**
+ * Generates a handler for an ApolloServer instance, used primarily for serverless hosting.
+ */
+export default function createHandler() {
+  const schema = createSchema();
+  const server = new ApolloServer({
+    schema,
+    csrfPrevention: true,
+    introspection: true,
+    cache: 'bounded',
+    plugins: [
+      ApolloServerPluginLandingPageLocalDefault({ embed: true }),
+    ],
+  });
+  return server.createHandler();
+}
